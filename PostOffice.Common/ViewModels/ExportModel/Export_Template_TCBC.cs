09e9d83cf7dfdb648486e0f773d43bdcb8eac2e0
pcking60 <thientt.bdst@gmail.com>
2017-11-05 22:22:40 +0700

    v1.3.5d

---
+++ PostOffice.Common/ViewModels/ExportModel/Export_Template_TCBC.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PostOffice.Common.ViewModels.ExportModel
+{
+    public class Export_Template_TCBC
+    {
+        public int? STT { get; set; }
+        public string ServiceName { get; set; }
+        public int? Quantity { get; set; }
+        public float? Fee { get; set; }
+        public decimal? TotalColection { get; set; }
+        public decimal? TotalPay { get; set; }
+        public decimal? Sale { get; set; }
+        public decimal? Tax { get; set; }
+        public decimal? EarnMoney { get; set; }
+    }
+}
